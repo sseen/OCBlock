698cd9107eaf86dd02310cb5381cf883ffb71f42
ssn <ssnfee@gmail.com>
2016-06-21 09:39:55 +0800

    Initial Commit

---
+++ OCBlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCBlock
+//
+//  Created by sseen on 16/6/21.
+//  Copyright © 2016年 sseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCBlock
+//
+//  Created by sseen on 16/6/21.
+//  Copyright © 2016年 sseen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
